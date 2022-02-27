71549a8a361f7a2e38c6d122cddfced171a6dc42
Josh Fang <jfang.96@gmail.com>
2022-02-27 14:55:48 -0800

    Add reset button to clear grid

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,3 +1,4 @@
+// Create 16x16 grid
 const container = document.querySelector('#container');
 
 rows = 16;
@@ -7,13 +8,22 @@ for (c = 0; c < rows*cols; c++) {
     const cell = document.createElement('div');
     cell.classList.add('cell');
     container.appendChild(cell);
-    cell.textContent = 'cell';
     cell.addEventListener('mouseover', () => {
         cell.style['background-color'] = 'blue';
     })
 }
 
-container.style['grid-template-columns'] = Array(cols).fill('1fr').join(' ');
+container.style['grid-template-columns'] = Array(cols).fill('20px').join(' ');
 
-container.style['grid-template-rows'] = Array(rows).fill('1fr').join(' ');
+container.style['grid-template-rows'] = Array(rows).fill('20px').join(' ');
 
+
+// Reset Button
+const resetBtn = document.querySelector('#resetBtn'); 
+resetBtn.addEventListener('click', () => {
+    const cells = document.getElementsByClassName('cell');
+    for (const cell of cells) {
+        cell.style['background-color'] = 'transparent';
+    }
+        
+});
