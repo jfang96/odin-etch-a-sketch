2652921094f227082a7278594666038d2b367922
Josh Fang <jfang.96@gmail.com>
2022-02-27 14:31:34 -0800

    Change color of div when hovering over cell

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -8,6 +8,12 @@ for (c = 0; c < rows*cols; c++) {
     cell.classList.add('cell');
     container.appendChild(cell);
     cell.textContent = 'cell';
+    cell.addEventListener('mouseover', () => {
+        cell.style['background-color'] = 'blue';
+    })
 }
 
-container.style['grid-template-columns'] = 'auto '.repeat(cols);
+container.style['grid-template-columns'] = Array(cols).fill('1fr').join(' ');
+
+container.style['grid-template-rows'] = Array(rows).fill('1fr').join(' ');
+
