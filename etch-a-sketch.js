20e435cea2223c74cba11c93fa6900f59ce4f8fb
Josh Fang <jfang.96@gmail.com>
2022-02-27 15:26:13 -0800

    Reset button prompts user for new grid size

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -4,26 +4,35 @@ const container = document.querySelector('#container');
 rows = 16;
 cols = 16;
 
-for (c = 0; c < rows*cols; c++) {
-    const cell = document.createElement('div');
-    cell.classList.add('cell');
-    container.appendChild(cell);
-    cell.addEventListener('mouseover', () => {
-        cell.style['background-color'] = 'blue';
-    })
+createGrid(rows, cols);
+
+function createGrid(rows, cols) {
+    for (c = 0; c < rows*cols; c++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        container.appendChild(cell);
+        cell.addEventListener('mouseover', () => {
+            cell.style['background-color'] = 'blue';
+        })
+    }
+
+    // container.style['grid-template-rows'] = Array(rows).fill('auto').join(' ');
+
+    container.style['grid-template-columns'] = Array(cols).fill('auto').join(' ');    
 }
-
-container.style['grid-template-columns'] = Array(cols).fill('20px').join(' ');
-
-container.style['grid-template-rows'] = Array(rows).fill('20px').join(' ');
 
 
 // Reset Button
 const resetBtn = document.querySelector('#resetBtn'); 
 resetBtn.addEventListener('click', () => {
-    const cells = document.getElementsByClassName('cell');
-    for (const cell of cells) {
-        cell.style['background-color'] = 'transparent';
-    }
-        
+    clearGrid();
+    rows = parseInt(prompt("Number of rows/columns (max: 100)", 16));
+    rows = Math.min(rows, 100); // Limit grid size to 100
+    createGrid(rows, rows);
 });
+
+function clearGrid() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }  
+}
